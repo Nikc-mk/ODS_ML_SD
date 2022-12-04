f5727943427a29765388c3f9b88b99d3746cc4d9
Nikolay <3kaal6@gmail.com>
2022-12-04 21:20:04 +1000

    сделал хэндлер для бота, который будет реагировать на все фото. подключил модель haarcascade_frontalface_default.xml, сделал функцию по выделению лица на фото

--- handlers/main_handlers.py
+++ handlers/main_handlers.py
@@ -1,6 +1,7 @@
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher import Dispatcher
 from aiogram import types
+from aiogram.types import ContentType
 
 start_buttons = ["Информация о боте", "Загрузить фото"]
 
@@ -15,5 +16,25 @@ async def cmd_start(message: types.Message):
     # отвечаем пользователю после старта
 
 
+async def cmd_download(message: types.Message):
+    name_user = message.from_user.first_name
+    await message.answer(f"{name_user} отправьте мне фотографию с людьми, я их обработаю и верну вам")
+
+
+async def cmd_media(message: types.Message):
+    name_user = message.from_user.first_name
+    await message.answer(f"{name_user} отправил фото")
+
+
+
+# отвечаем на все неизвестные сообщения
+async def cmd_answer_all(message: types.Message):
+    await message.answer('Не знаю такой команды.\nВоспользуйтесь командой /start')
+
+
 def register_handlers_users(dp: Dispatcher):
     dp.register_message_handler(cmd_start, commands='start')
+    dp.register_message_handler(cmd_download, commands='download')
+    dp.register_message_handler(cmd_download, Text(equals=start_buttons[1], ignore_case=True))
+    dp.register_message_handler(cmd_media, content_types=ContentType.PHOTO)
+    dp.register_message_handler(cmd_answer_all)
---
+++ models/model_1_opencv.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+
+# загружаем изображение, с помощью COLOR_BGR2GRAY делаем изображение черно-белым
+image = cv.imread("photo_2022.jpg", cv.COLOR_BGR2GRAY)
+
+
+def change_photo(image):
+    # делаем изображение более контрастным
+    # image = cv.equalizeHist(image)
+
+    face_cascade = cv.CascadeClassifier("cascades/haarcascade_frontalface_default.xml")
+
+    faces = face_cascade.detectMultiScale(image)
+    for (x, y, w, h) in faces:
+        center = (x + w // 2, y + h // 2)
+        frame = cv.ellipse(image, center, (w // 2, h // 2), 0, 0, 360, (255, 0, 255), 4)
+        faceROI = image[y:y + h, x:x + w]
+        # -- In each face, detect eyes
+
+    cv.imshow('Capture - Face detection', frame)
+    cv.waitKey(0)
+
+
+change_photo(image=image)
